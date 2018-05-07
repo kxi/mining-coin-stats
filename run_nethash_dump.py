a57fbd34bf46b46bc88319538563a9591259d94b
Kai Xi <kaixi.work@gmail.com>
2018-05-07 17:15:13 +1000

    Add Syscall Timeout

--- run_nethash_dump.py
+++ run_nethash_dump.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def main():
     coin = sys.argv[1]
     interval_sec = float(sys.argv[2])
-    timeout_sec = 180
+    timeout_sec = 60
     sleep_sec = interval_sec - timeout_sec
 
     while 1:
@@ -15,14 +15,10 @@ def main():
         now = time.time()
 
         try:
-#            process = subprocess.call("python check_network_hash.py update {}".format(coin, interval_sec), stderr=subprocess.STDOUT, timeout = timeout_sec)
-
-            system_command=subprocess.Popen("python check_network_hash.py update {}".format(coin, interval_sec), shell=True, stdout=subprocess.PIPE)
-            stdout, stderr = system_command.communicate()
-            print(stdout)
+            process = subprocess.run("python check_network_hash.py update {} {}".format(coin, interval_sec), stderr=subprocess.STDOUT, shell=True, timeout = timeout_sec)
 
         except Exception as e:
-            print(e)
+            print("[Timeout]: {}".format(e))
 
         print("Finish Dump Nethash @ {}".format((str(datetime.now()))))
         print("****** It Takes {0:.1f} Second to Finish Dump Nethash! ******".format(time.time() - now))
