acb5cd2bee158195cca191e416ef58eac8e6d8b7
Kai Xi <kaixi.work@gmail.com>
2018-05-08 10:47:53 +1000

    Add Initialize Nethash Info File

--- check_network_hash.py
+++ check_network_hash.py
@@ -69,7 +69,10 @@ def main():
                     continue
 
                 fname = os.path.join(COIN_NETHASH_INFO_PATH, coin+"_nethash_info.csv")
-
+                
+                if not os.path.isfile(fname):
+                    df_init(recent_block_id, coin_explorer_url, fname)
+                
                 df = pd.read_csv(fname)
                 row = df.size
 
